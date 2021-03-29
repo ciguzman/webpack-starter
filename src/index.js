d07140d49ebf3d6875fd4ab042a7b87d7ac04216
CesarG <ciguzman201@gmail.com>
2021-03-28 23:59:30 -0600

    docs actualizado

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@
 import { saludar } from './js/componentes'
 import './styles.css'
 
-const nombre = 'Cesar!!!';
+
+const nombre = 'Laura';
 saludar(nombre);
 
 
--- src/js/componentes.js
+++ src/js/componentes.js
@@ -2,9 +2,9 @@ import '../css/componentes.css';
 
 //export para poder usarla fuera de este archivo
 export const saludar = ( nombre ) =>{
-    console.log('Creando h1...');
+    // console.log('Creando h1 en el HTML');
 
     const h1 = document.createElement('h1');
-    h1.innerText = `Hola ${nombre} como estas`;
+    h1.innerText = `Hola ${nombre}, Cesar te ama mucho :3`;
     document.body.append(h1);
 }
